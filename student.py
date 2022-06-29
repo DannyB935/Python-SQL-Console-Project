fd3341e7de8a6983560cdc53465dd38b920b2089
Daniel Bustos Ruiz <daniel.bustos4669@alumnos.udg.mx>
2022-06-29 18:32:13 -0500

    Created the student class and his init method

---
+++ student.py
@@ -0,0 +1,11 @@
+
+class Student:
+
+    def __init__(self):
+
+        self.name = ''
+        self.lastName = ''
+        self.age = 0
+        self.code = ''
+        self.semester = 0
+        self.degree = ''
