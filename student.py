d69934e77a685a6e6122cbbfe0a03f6a33c9990d
Daniel Bustos Ruiz <daniel.bustos4669@alumnos.udg.mx>
2022-06-29 18:54:29 -0500

    Added the setters and getters methods for the student class

--- student.py
+++ student.py
@@ -9,3 +9,43 @@ class Student:
         self.code = ''
         self.semester = 0
         self.degree = ''
+
+    #Setters
+
+    def setName(self, n):
+        self.name = n
+
+    def setLastName(self, l):
+        self.lastName = l
+
+    def setAge(self, a):
+        self.age = a
+
+    def setCode(self, c):
+        self.code = c
+
+    def setSemester(self, s):
+        self.semester = s
+
+    def setDegree(self, d):
+        self.degree = d
+
+    #Getters
+
+    def getName(self):
+        return self.name
+
+    def getLastName(self):
+        return self.lastName
+
+    def getAge(self):
+        return self.age
+
+    def getCode(self):
+        return self.code
+
+    def getSemester(self):
+        return self.semester
+
+    def getDregree(self):
+        return self.degree
