8553fff296c6304086549b57db42de5457f0b53b
Daniel Bustos Ruiz <daniel.bustos935@hotmail.com>
2022-07-01 14:01:24 -0500

    Added try-except to initialize the database connection

--- dataBase.py
+++ dataBase.py
@@ -4,14 +4,19 @@ class DataBase:
 
     def __init__(self):
 
-        self.studentDb = mysql.connector.connect(
+        try:
 
-            host = 'localhost',
-            user = 'root',
-            passwd = '',
-            database = 'students'
+            self.studentDb = mysql.connector.connect(
 
-        )
+                host = 'localhost',
+                user = 'root',
+                passwd = '',
+                database = 'students'
+
+            )
+        except:
+
+            print("Cannot connect to 'students' database, DataBase class")
 
     def checkConnection(self):
 
